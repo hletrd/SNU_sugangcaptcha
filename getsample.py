71d3bb8d982a09db087d019560fc84871e284d8b
Jiyong Youn <01@0101010101.com>
2016-03-14 20:41:40 +0900

    Set filename as random

--- getsample.py
+++ getsample.py
@@ -1,4 +1,6 @@
 import httplib
+import string
+import random
 
 headers = {'User-agent': 'Python'}
 
@@ -7,6 +9,6 @@ for i in range(100):
 	conn.request('GET', '/sugang/ca/number.action', '', headers)
 	resp = conn.getresponse()
 	rest = resp.read()
-	f = open('read/' + str(i) + '.png', 'wb')
+	f = open('read/' + ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(10)) + '.png', 'wb')
 	f.write(rest)
 	print 'Downloaded ' + str(i)
